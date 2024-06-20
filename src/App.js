43844dbe0ba8990134d6b4d3e3f5e038dcd282fa
Mohammed Tafsir <104312689+way3-3@users.noreply.github.com>
2024-06-20 22:59:33 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       className="App"
       style={{
         height:
-          window.location.href === "http://localhost:3000/chat"
+          window.location.href === "https://back-xcia.onrender.com/chat"
             ? "calc(100vh - 2rem)"
             : "auto",
       }}
