0008010baf3ad026f016815302baca386e66f4dd
Mohammed Tafsir <104312689+way3-3@users.noreply.github.com>
2024-06-20 20:24:48 +0600

    Update UserRequests.js

--- src/api/UserRequests.js
+++ src/api/UserRequests.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://back-1-ekqe.onrender.com" });
+const API = axios.create({ baseURL: "https://back-xcia.onrender.com" });
 
 // API.interceptors.request.use((req) => {
 //     if (localStorage.getItem('profile')) {
