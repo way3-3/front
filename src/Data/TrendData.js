46301594c5c4d712aad0f9353ca0b65bef5d26c1
way3-3 <mohammedtafsir3@gmail.com>
2024-06-20 18:39:44 +0600

    frontcommit

---
+++ src/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData = [
+  {
+    name: "Cryptocurrency Crash",
+    shares: 120, // Adjust shares to reflect perceived popularity
+  },
+  {
+    name: "Monkeypox Outbreak",
+    shares: 105,
+  },
+  {
+    name: "New iPhone Rumors",
+    shares: 98,
+  },
+  {
+    name: "Top Gun: Maverick Success",
+    shares: 82,
+  },
+  {
+    name: "AI Art Generator",
+    shares: 77,
+  },
+];
