f0e4e30678d9724b0a1648ffc58d69ac8ad6c62d
Mohammed Tafsir <104312689+way3-3@users.noreply.github.com>
2024-06-20 20:23:06 +0600

    Update AuthRequests.js

--- src/api/AuthRequests.js
+++ src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://back-1-ekqe.onrender.com' });
+const API = axios.create({ baseURL: 'https://back-xcia.onrender.com' });
 
 export const logIn= (formData)=> API.post('/auth/login',formData);
 
