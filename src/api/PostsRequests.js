9875c3427420b9b1146afb8b5274bc4ad9b0e08e
Mohammed Tafsir <104312689+way3-3@users.noreply.github.com>
2024-06-20 20:24:10 +0600

    Update PostsRequests.js

--- src/api/PostsRequests.js
+++ src/api/PostsRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'https://back-1-ekqe.onrender.com' });
+const API = axios.create({ baseURL: 'https://back-xcia.onrender.com' });
 
 // API.interceptors.request.use((req) => {
 //     if (localStorage.getItem('profile')) {
